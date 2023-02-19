f070ed381b141d8cb332bf628f7137f0e4d255de
Gino Amaro <emzero1@gmail.com>
2023-02-19 20:20:57 -0300

    Seccion 6 Terminado

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from '../app-routing.module';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// import { ChartsModule } from "ng2-charts";
+
+import { ComponentsModule } from '../components/components.module';
 import { SharedModule } from '../shared/shared.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
 
 
 
@@ -18,16 +22,19 @@ import { PagesComponent } from './pages.component';
     Grafica1Component,
     PagesComponent
   ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    AppRoutingModule
-  ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
     PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule,
+    FormsModule,
+    ComponentsModule,
+    // ChartsModule
   ]
 })
 export class PagesModule { }
